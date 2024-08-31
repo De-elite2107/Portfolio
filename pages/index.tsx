2cdd8e606c45eb3d7ce92b14b959e127ca546e0b
Delight <delightadediran21@gmail.com>
2024-08-31 22:38:01 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ const Home = () => {
   const [isLargerThan1000] = useMediaQuery("(min-width: 1000px)");
   const [isLargerThan400] = useMediaQuery("(min-width: 400px)");
   const [isOpen, setOpen] = useState(false);
+  const [stylesLoaded, setStylesLoaded] = useState(false);
   const menuRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -39,111 +40,126 @@ const Home = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setStylesLoaded(true);
+    }, 1000);
+  }, []);
   const [tabIndex, setTabIndex] = useState(0);
   return (
-    <Box h="100%" bg="#081f37">
-      <Tabs transition="ease-in-out" onChange={(index) => setTabIndex(index)} index={tabIndex}>
-        <Box
-        position="fixed"
-        top={0}
-        zIndex={100}
-        w="100%"
-        bg="#081f37"
-        >
-          <Flex
-            alignItems="center"
-            justifyContent="space-between"
-            color="#5fc9f3"
-          >
-            <Text p="1rem" fontSize="24px">
-              <FancyText gradient={{
-                type: "linear",
-                from: "#1e549f",
-                to: "#5fc9f3",
-              }}>
-                De-elite Technologies
-              </FancyText>
-            </Text>
-            {isLargerThan1000 ? (
-              <TabList>
-                <Tab w="10rem" onClick={()=>{setTabIndex(0)}}>Home</Tab>
-                <Tab w="10rem" onClick={()=>{setTabIndex(1)}}>Resume</Tab>
-                <Tab w="10rem" onClick={()=>{setTabIndex(2)}}>Portfolio</Tab>
-                <Tab w="10rem" onClick={()=>{setTabIndex(3)}}>Contacts</Tab>
-              </TabList>
-            ) : (
-              <Box
-
-                ref={menuRef}
+    <>
+      {stylesLoaded ? (
+        <Box h="100%" bg="#081f37">
+          <Tabs transition="ease-in-out" onChange={(index) => setTabIndex(index)} index={tabIndex}>
+            <Box
+            position="fixed"
+            top={0}
+            zIndex={100}
+            w="100%"
+            bg="#081f37"
+            >
+              <Flex
+                alignItems="center"
+                justifyContent="space-between"
+                color="#5fc9f3"
               >
-                <Menu>
-                  <MenuButton _hover={{"bg" : "none"}} _active={{"bg" : "none"}} bg="none" as={IconButton} icon={<Hamburger toggled={isOpen} toggle={setOpen} color="#2e79ba"/>}/>
-                  <MenuList className="menulist" bg="#1e549f" border="none">
-                    <MenuItem 
-                    _hover={{"bg" : "#081f37"}} 
-                    onClick={() => {
-                      setOpen(false)
-                      setTabIndex(0)
-                    }} 
-                    bg={tabIndex === 0 ? "#081f37" : ""}
-                    >
-                      Home
-                    </MenuItem>
-                    <MenuItem 
-                    _hover={{"bg" : "#081f37"}} 
-                    onClick={() => {
-                      setOpen(false)
-                      setTabIndex(1)
-                    }} 
-                    bg={tabIndex === 1 ? "#081f37" : ""}
-                    >
-                      Resume
-                    </MenuItem>
-                    <MenuItem 
-                    _hover={{"bg" : "#081f37"}} 
-                    onClick={() => {
-                      setOpen(false)
-                      setTabIndex(2)
-                    }} 
-                    bg={tabIndex === 2 ? "#081f37" : ""}
-                    >
-                      Portfolio
-                    </MenuItem>
-                    <MenuItem 
-                    _hover={{"bg" : "#081f37"}} 
-                    onClick={() => {
-                      setOpen(false)
-                      setTabIndex(3)
-                    }} 
-                    bg={tabIndex === 3 ? "#081f37" : ""}
-                    >
-                      Contacts
-                    </MenuItem>
-                  </MenuList>
-                </Menu>
-              </Box>
-            )}
-          </Flex>
+                <Text p="1rem" fontSize="24px">
+                  <FancyText gradient={{
+                    type: "linear",
+                    from: "#1e549f",
+                    to: "#5fc9f3",
+                  }}>
+                    De-elite Technologies
+                  </FancyText>
+                </Text>
+                {isLargerThan1000 ? (
+                  <TabList>
+                    <Tab w="10rem" onClick={()=>{setTabIndex(0)}}>Home</Tab>
+                    <Tab w="10rem" onClick={()=>{setTabIndex(1)}}>Resume</Tab>
+                    <Tab w="10rem" onClick={()=>{setTabIndex(2)}}>Portfolio</Tab>
+                    <Tab w="10rem" onClick={()=>{setTabIndex(3)}}>Contacts</Tab>
+                  </TabList>
+                ) : (
+                  <Box
+
+                    ref={menuRef}
+                  >
+                    <Menu>
+                      <MenuButton _hover={{"bg" : "none"}} _active={{"bg" : "none"}} bg="none" as={IconButton} icon={<Hamburger toggled={isOpen} toggle={setOpen} color="#2e79ba"/>}/>
+                      <MenuList className="menulist" bg="#1e549f" border="none">
+                        <MenuItem 
+                        _hover={{"bg" : "#081f37"}} 
+                        onClick={() => {
+                          setOpen(false)
+                          setTabIndex(0)
+                        }} 
+                        bg={tabIndex === 0 ? "#081f37" : ""}
+                        >
+                          Home
+                        </MenuItem>
+                        <MenuItem 
+                        _hover={{"bg" : "#081f37"}} 
+                        onClick={() => {
+                          setOpen(false)
+                          setTabIndex(1)
+                        }} 
+                        bg={tabIndex === 1 ? "#081f37" : ""}
+                        >
+                          Resume
+                        </MenuItem>
+                        <MenuItem 
+                        _hover={{"bg" : "#081f37"}} 
+                        onClick={() => {
+                          setOpen(false)
+                          setTabIndex(2)
+                        }} 
+                        bg={tabIndex === 2 ? "#081f37" : ""}
+                        >
+                          Portfolio
+                        </MenuItem>
+                        <MenuItem 
+                        _hover={{"bg" : "#081f37"}} 
+                        onClick={() => {
+                          setOpen(false)
+                          setTabIndex(3)
+                        }} 
+                        bg={tabIndex === 3 ? "#081f37" : ""}
+                        >
+                          Contacts
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Box>
+                )}
+              </Flex>
+            </Box>
+            <TabPanels>
+              <TabPanel>
+                <Homepage/>
+              </TabPanel>
+              <TabPanel>
+                <Resume/>
+              </TabPanel>
+              <TabPanel>
+                <Portfolio/>
+              </TabPanel>
+              <TabPanel>
+                <ContactUs/>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+          <Box position={isLargerThan400 ? "fixed" : "relative"} display="block" w="100%" zIndex={100} bottom={0} bg="#081f37">
+            <Text color="#5fc9f3" p="1.5rem" fontSize="14px">&copy; 2024 De-elite Technologies. All rights reserved.</Text>
+          </Box>
         </Box>
-        <TabPanels>
-          <TabPanel>
-            <Homepage/>
-          </TabPanel>
-          <TabPanel>
-            <Resume/>
-          </TabPanel>
-          <TabPanel>
-            <Portfolio/>
-          </TabPanel>
-          <TabPanel>
-            <ContactUs/>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-      <Box position={isLargerThan400 ? "fixed" : "relative"} display="block" w="100%" zIndex={100} bottom={0} bg="#081f37">
-        <Text color="#5fc9f3" p="1.5rem" fontSize="14px">&copy; 2024 De-elite Technologies. All rights reserved.</Text>
-      </Box>
-    </Box>
+      ) : (
+        <Box bg="#081f37" height="100vh">
+          <Text mt="40vh" color="white" p="2rem" fontSize="24px" textAlign="center">
+            Loading...
+          </Text>
+        </Box>
+      )}
+    </>
   );
 };
 export default Home;
