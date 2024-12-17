8374a839be12c6d8ceed733c9f6b7667b8dd9b43
theroyalimage <delightadediran21@gmail.com>
2024-12-17 20:19:47 +0100

    Push

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -49,11 +49,27 @@ const Portfolio = () => {
             <Flex flexWrap="wrap" gap={8}>
                 <Card
                 Image='./images/portfolio/adeptbloc.png'
+                Name="Church Management System"
+                Type="API"
+                Duration="2 days"
+                URL="https://cmsserver.up.railway.app/api/"
+                Details='Our Church Management System API offers seamless integration for managing congregational data, events, and communications, enhancing community engagement and operational efficiency. Designed for flexibility and scalability, it empowers churches to streamline their administrative tasks and foster stronger connections within their communities...'
+                />
+                <Card
+                Image='./images/portfolio/crms.png'
+                Name="Course Resources Management System"
+                Type="LMS"
+                Duration="3 weeks"
+                URL="https://crmsys.netlify.app/"
+                Details='Our Course Resource Management System streamlines the organization and accessibility of educational materials, enhancing the learning experience for students and educators alike. With intuitive navigation and robust features, it empowers users to efficiently manage resources and collaborate effectively...'
+                />
+                <Card
+                Image='./images/portfolio/adeptbloc.png'
                 Name="AdeptBloc's Landing Page"
                 Type="Landing Page"
                 Duration="3 days"
                 URL="https://adeptbloc.netlify.app/"
-                Details={`A platform where interns and tutors connect (Site is in progress!)...`}
+                Details='Discover exciting opportunities on our Virtual Internship web page, designed to connect students with valuable remote work experiences across various industries. With user-friendly navigation and comprehensive resources, we empower aspiring professionals to gain practical skills and enhance their career prospects from anywhere. (Site is in progress!)...'
                 />
                 <Card
                 Image='./images/portfolio/portfolio.png'
@@ -61,7 +77,7 @@ const Portfolio = () => {
                 Type="Portfolio"
                 Duration="3 days"
                 URL="https://de-elite.netlify.app/"
-                Details="A platform built to display my niches, experience and skill level..."
+                Details='A platform built to display my niches, experience and skill level...'
                 />
                 <Card
                 Image='./images/portfolio/propertyco.png'
@@ -76,7 +92,7 @@ const Portfolio = () => {
                 Name="Remkay's Web Page"
                 Type="School Webpage"
                 Duration="3 months"
-                URL="https://www.propertyco.ng/"
+                URL="https://remkayschools.pythonanywhere.com"
                 Details='Explore our vibrant school website, where academic excellence meets a nurturing community. Discover resources, events, and insights that empower students and parents alike to thrive in a dynamic learning environment...'
                 />
             </Flex>
