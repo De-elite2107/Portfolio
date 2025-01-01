bf20fbbf678322ddf7f8956f77e39e9dfb0d764a
theroyalimage <delightadediran21@gmail.com>
2025-01-01 20:50:25 +0100

    update portfolio

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -52,7 +52,7 @@ const Portfolio = () => {
                 Name="Church Management System"
                 Type="API"
                 Duration="2 days"
-                URL="https://cmsserver.up.railway.app/api/"
+                URL="https://cmsserver.up.railway.app/"
                 Details='Our Church Management System API offers seamless integration for managing congregational data, events, and communications, enhancing community engagement and operational efficiency. Designed for flexibility and scalability, it empowers churches to streamline their administrative tasks and foster stronger connections within their communities...'
                 />
                 <Card
