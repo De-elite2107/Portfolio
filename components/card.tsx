4b672d639f0b135ebc233089bebd0f4c7d383e09
Delight <delightadediran21@gmail.com>
2025-10-27 08:25:18 +0100

    ratio

--- components/card.tsx
+++ components/card.tsx
@@ -1,49 +1,71 @@
-import { Box, Flex, Img, Link, Text } from '@chakra-ui/react'
-import React from 'react'
+import { AspectRatio, Box, Flex, Grid, GridItem, Img, Link, Text } from "@chakra-ui/react";
+import React from "react";
 
 type Icard = {
-    Image?: string,
-    Video?: string,
-    Name: string,
-    Type: string,
-    Duration: string,
-    URL: string,
-    Details: string,
-}
+  Image?: string;
+  Name: string;
+  Type: string;
+  Duration: string;
+  URL: string;
+  Details: string;
+};
 
-const Card = (props : Icard) => {
+const Card = (props: Icard) => {
   return (
-    <Box color="white" mb="3rem" w="100%">
-        <Box
-        mb="1rem"
-        w="20rem"
-        h="11rem"
-        >
-            {props.Image && (<Img src={props.Image} alt='Card Img' w="100%" h="100%"/>)}
-            {props.Video && (<video src={props.Video} muted/>)}
-        </Box>
-        <Flex gap={2} alignItems="start" mb="0.5rem">
-            <Text color="#5fc9f3" w="5rem">Title:</Text>
-            <Text>{props.Name}</Text>
-        </Flex>
-        <Flex gap={2} alignItems="start" mb="0.5rem">
-            <Text color="#5fc9f3" w="5rem">Type:</Text>
-            <Text>{props.Type}</Text>
-        </Flex>
-        <Flex gap={2} alignItems="start" mb="0.5rem">
-            <Text color="#5fc9f3" w="5rem">Duration:</Text>
-            <Text>{props.Duration}</Text>
-        </Flex>
-        <Flex gap={2} alignItems="start">
-            <Text color="#5fc9f3" w="5rem">URL:</Text>
-            <Text><Link textDecoration="underline" href={props.URL} target='_blank'>Link to Site</Link></Text>
-        </Flex>
-        <Flex alignItems="start" flexWrap="wrap" gap={2} mb="0.5rem">
-            <Text color="#5fc9f3" w="5rem">Details:</Text>
-            <Text>{props.Details}</Text>
-        </Flex>
+    <Box color="white" mb="3rem" w={{base: "100%" , md: "23rem"}}>
+      <AspectRatio ratio={16/9} mb="0.5rem">
+        {props.Image && (
+          <Img src={props.Image} alt="Card Img" w="100%" h="100%" />
+        )}
+      </AspectRatio>
+      <Grid gap={2} alignItems="start" templateColumns={{base: "repeat(1, 1fr)", md: "repeat(5, 1fr)"}}>
+        <GridItem mb="0.5rem">
+          <Text color="#5fc9f3" w="5rem">
+            Title:
+          </Text>
+        </GridItem>
+        <GridItem mb="0.5rem" colSpan={{base: 1, md: 4}}>
+          <Text>{props.Name}</Text>
+        </GridItem>
+        <GridItem mb="0.5rem">
+          <Text color="#5fc9f3" w="5rem">
+            Type:
+          </Text>
+        </GridItem>
+        <GridItem mb="0.5rem" colSpan={{base: 1, md: 4}}>
+          <Text>{props.Type}</Text>
+        </GridItem>
+        <GridItem mb="0.5rem">
+          <Text color="#5fc9f3" w="5rem">
+            Duration:
+          </Text>
+        </GridItem>
+        <GridItem mb="0.5rem" colSpan={{base: 1, md: 4}}>
+          <Text>{props.Duration}</Text>
+        </GridItem>
+        <GridItem>
+          <Text color="#5fc9f3" w="5rem">
+            URL:
+          </Text>
+        </GridItem>
+        <GridItem colSpan={{base: 1, md: 4}}>
+          <Text>
+            <Link textDecoration="underline" href={props.URL} target="_blank">
+              Link to Site
+            </Link>
+          </Text>
+        </GridItem>
+        <GridItem mb="0.5rem">
+          <Text color="#5fc9f3" w="5rem">
+            Details:
+          </Text>
+        </GridItem>
+        <GridItem mb="0.5rem" colSpan={{base: 1, md: 4}}>
+          <Text>{props.Details}</Text>
+        </GridItem>
+      </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
