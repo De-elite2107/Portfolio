ebadefd6b6db3b7ac0980743b21c06beab1b96fc
Delight <delightadediran21@gmail.com>
2025-10-30 03:19:19 +0100

    protection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    const apiKey = 'YOUR_API_KEY';
+    const apiKey = 'NqBVVemHeec5UEpwXwlPZuNXownfCEPw';
     const endpoint = 'http://localhost:8000/api/monitor/activity';
 
     fetch(endpoint, {
